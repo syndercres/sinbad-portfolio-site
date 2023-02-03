9ca243e54a63367ba185ef3545cb0a11be96faec
syndercombecreswell <syndercombecreswell@gmail.com>
2023-02-03 16:28:43 +0000

    image

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,6 +8,7 @@ export default function AboutMe():JSX.Element {
 
             <div>
                 <p>Hi I'm Sinbad! I'm an Electrical Engineer, Software Developer and extreme sports enthusiast</p>
+                <img src="images/skate_photo_1.jpeg" alt="Me skating"></img>
             </div>
         </div>
     )
