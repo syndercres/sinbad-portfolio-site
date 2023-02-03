076a3d251ea5e94a6cce5f9e75a900d9f47c0bc9
syndercombecreswell <syndercombecreswell@gmail.com>
2023-02-03 14:20:00 +0000

    implimented project list

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,10 +1,17 @@
-
+import { projectList } from "../utils/ProjectList"
 
 
 export default function ProjectsPage():JSX.Element{
     return(
         <div>
             <h1>this is the projects page</h1>
+            <div>
+                {projectList.map((project)=> {return(
+                   <div key={project.name}> 
+                    <h1>{project.name}</h1>
+                    </div>
+                )})}
+            </div>
         </div>
     )
 }
