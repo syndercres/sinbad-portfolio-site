9a0b8b60409d6e3ff8d3cdb8ba87fd1f0db13b6c
syndercombecreswell <syndercombecreswell@gmail.com>
2023-02-03 14:15:39 +0000

    project list

---
+++ src/utils/ProjectList.tsx
@@ -0,0 +1,17 @@
+
+
+export interface IProject {
+    name: string,
+    siteLink: string,
+    gitHubLink: string,
+    info: string
+}
+
+export const projectList: IProject [] = [
+    {
+        name: "breed battle",
+        siteLink:"www.dog.com",
+        gitHubLink:"www.git-dog.com",
+        info:"breed battle project"
+    }
+]
