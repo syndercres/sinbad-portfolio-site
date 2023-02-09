8d549086fddc1a4b5989c5c476cdc15ba05a7ecc
syndercombecreswell <syndercombecreswell@gmail.com>
2023-02-09 15:43:20 +0000

    css

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,6 +1,6 @@
-import skate from "../images/skate.png";
-import "../component-styles/AboutMe.css";
 
+import "../component-styles/AboutMe.css";
+import { funPhotos } from "../utils/ProjectList";
 export default function AboutMe(): JSX.Element {
   return (
     <div>
@@ -11,7 +11,14 @@ export default function AboutMe(): JSX.Element {
           Hi I'm Sinbad! I'm an Electrical Engineer, Software Developer and
           extreme sports enthusiast
         </p>
-        <img src={skate} alt="Me skating"></img>
+  
+      </div>
+      <div className="photo-grid">
+        {funPhotos.map((photo)=>{
+          return(
+            <img className="photo-item box" key={photo} src={photo} alt={photo} />
+          )
+        })}
       </div>
     </div>
   );
